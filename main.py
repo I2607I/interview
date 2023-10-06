d37da9e80478239e8004f9c98d845fe67cdc3589
Игорь Половко <bujhm2607@yandex.ru>
2023-10-06 19:27:45 +0700

    settings

--- main.py
+++ main.py
@@ -1,38 +1,50 @@
 import webbrowser
 from pynput.keyboard import Key, Controller
-
 import time
 import sys
+import os
+
+import settings as sts
+
+dict_time = {'tech': [sts.tech_time], 'algo': [sts.algo_time], 'interview': [sts.interview_tech_time, sts.interview_algo_time]}
+dict_number = {'tech': [sts.tech_number], 'algo': [sts.algo_number], 'interview': [sts.interview_tech_number, sts.interview_algo_number]}
+
+
 time_start = time.time()
 seconds = 0
 minutes = 0
-flag = False
+flag_start = False
 while True:
     try:
-        if flag is False:
+        if flag_start is False:
             a = input(">>> ")
-            if a == 'start algo':
+            if a == 'algo':
                 # алгоритмический собес
                 webbrowser.open("input.py")
-                flag = True
-            elif a == 'start tech':
+                flag_start = True
+            elif a == 'tech':
                 # технический собес
                 webbrowser.open("input.py")
-                flag = True
-            elif a == 'start interview':
+                flag_start = True
+            elif a == 'interview':
                 # полный собес
                 webbrowser.open("input.py")
-                flag = True
+                flag_start = True
             elif a == 'exit':
                 break
-        if flag is True:
+            if flag_start:
+                    print(zip(dict_time[a],dict_number[a]))
+                    minutes = sum([i*j for i,j in zip(dict_time[a],dict_number[a])])
+        if flag_start is True:
+            os.system('clear')
+            sys.stdout.write(f"The {a} interview is on!\n")
             sys.stdout.write("\r{minutes} Minutes {seconds} Seconds".format(minutes=minutes, seconds=seconds))
-            sys.stdout.flush()
+            # sys.stdout.flush()
             time.sleep(1)
-            seconds = int(time.time() - time_start) - minutes * 60
-            if seconds >= 60:
-                minutes += 1
-                seconds = 0
+            seconds -= 1
+            if seconds <= 0:
+                minutes -= 1
+                seconds = 60
         
     except KeyboardInterrupt as e:
         break
---
+++ settings.py
@@ -0,0 +1,11 @@
+# settings algo
+algo_number = 2
+algo_time = 15
+# settings tech
+tech_number = 10
+tech_time = 1
+# settings interview
+interview_algo_number = 2
+interview_algo_time = 15
+interview_tech_number = 5
+interview_tech_time = 1
